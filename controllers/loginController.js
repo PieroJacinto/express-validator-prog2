6142ff6d008872727bbf8a92051e0d3baab9b43c
PieroJacinto <pierojacinto@gmail.com>
2024-06-03 10:22:54 -0300

    login y register complete

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs');
 const db = require('../database/models');
 //requerimos express validator y validationResult:
+const { validationResult } = require("express-validator");
 
 
 const op = db.Sequelize.Op;
@@ -12,25 +13,32 @@ let loginController = {
     },
     login: function(req, res){
         //obtenemos los restultados de las validaciones       
-        
+        const resultValidation = validationResult(req)
         // preguntamos si hay errores y si los hay los enviamos a la vista, junto con lo q venia en el body         
-      
-        // Buscamos el usuario que se quiere loguear.
-        db.User.findOne({
-            where: [{email: req.body.email}]
-        })
-        .then( function ( user ) {
-            //Seteamos la session con la info del usuario
-            req.session.user = user;          
-            //Si tildó recordame => creamos la cookie.
-            if(req.body.rememberme != undefined){
-                res.cookie('userId', user.id, { maxAge: 1000 * 60 * 100})
-            }
-            return res.redirect('/');            
-        })
-        .catch( function(e) {
-            console.log(e)
-        })
+        if(!resultValidation.isEmpty()){
+            console.log("resultValidation:", JSON.stringify(resultValidation,null,4));
+            return res.render("login", { 
+                errors: resultValidation.mapped(),
+                oldData: req.body
+             })
+        } else {
+            // Buscamos el usuario que se quiere loguear.
+            db.User.findOne({
+                where: [{email: req.body.email}]
+            })
+            .then( function ( user ) {
+                //Seteamos la session con la info del usuario
+                req.session.user = user;          
+                //Si tildó recordame => creamos la cookie.
+                if(req.body.rememberme != undefined){
+                    res.cookie('userId', user.id, { maxAge: 1000 * 60 * 100})
+                }
+                return res.redirect('/');            
+            })
+            .catch( function(e) {
+                console.log(e)
+            })
+        }
     },
     logout: function(req,res){
         //Destruir la sessión
--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs');
 const db = require('../database/models');
 //requerimos express validator y validationResult:
+const { validationResult } = require("express-validator");
 
 
 let registerController = {
@@ -9,27 +10,33 @@ let registerController = {
         return res.render('register');
     },
     store: function (req, res) {
-         //obtenemos los restultados de las validaciones
-         
-         // preguntamos si hay errores y si los hay los enviamos a la vista, junto con lo q venia en el body
-         
-
-        // Guardar un usuario en la db
-        const user = {
-            name: req.body.name,
-            email: req.body.email,
-            password: bcrypt.hashSync(req.body.password, 10), 
-        };
-        //creamos el usuario
-        db.User
-            .create(user)
-            .then(function (user) {
-                return res.redirect("/login");
+        //obtenemos los restultados de las validaciones       
+        const resultValidation = validationResult(req)
+        // preguntamos si hay errores y si los hay los enviamos a la vista, junto con lo q venia en el body         
+        if (!resultValidation.isEmpty()) {
+            console.log("resultValidation:", JSON.stringify(resultValidation, null, 4));
+            return res.render("register", {
+                errors: resultValidation.mapped(),
+                oldData: req.body
             })
-            .catch(function (err) {
-                console.log("Error al guardar el usuario", err);
-            });
-    },
+        } else {
 
+            // Guardar un usuario en la db
+            const user = {
+                name: req.body.name,
+                email: req.body.email,
+                password: bcrypt.hashSync(req.body.password, 10),
+            };
+            //creamos el usuario
+            db.User
+                .create(user)
+                .then(function (user) {
+                    return res.redirect("/login");
+                })
+                .catch(function (err) {
+                    console.log("Error al guardar el usuario", err);
+                });
+        }
+    },
 };
 module.exports = registerController;
